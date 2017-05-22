7e06203ee52562777457880444def3e08ab9d1de
Travis Chase <travis@travischase.me>
2017-05-22 14:59:49 -0500

    Change to before_action

--- app/controllers/user_impersonate/application_controller.rb
+++ app/controllers/user_impersonate/application_controller.rb
@@ -1,9 +1,4 @@
 module UserImpersonate
   class ApplicationController < ::ApplicationController
-  	[:before, :after, :around].each do |callback|
-  	  define_method "#{callback}_filter" do |*names, &blk|
-        send("#{callback}_action", *names, &blk)
-      end
-  	end
   end
 end
