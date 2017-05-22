3c5d1fac870c45aefd153eb57d7c196d39ddd4f8
Travis Chase <travis@travischase.me>
2017-05-22 14:56:23 -0500

    Support _filter callbacks

--- app/controllers/user_impersonate/application_controller.rb
+++ app/controllers/user_impersonate/application_controller.rb
@@ -1,4 +1,9 @@
 module UserImpersonate
   class ApplicationController < ::ApplicationController
+  	[:before, :after, :around].each do |callback|
+  	  define_method "#{callback}_filter" do |*names, &blk|
+        send("#{callback}_action", *names, &blk)
+      end
+  	end
   end
 end
